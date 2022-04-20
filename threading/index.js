bd14de5a5af3c0e1c97f5b6092c37cb1fb35326e
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-04-20 08:56:14 +0500

    changing thread-pool size in our platform

--- threading/index.js
+++ threading/index.js
@@ -1,5 +1,9 @@
+
+process.env.UV_THREADPOOL_SIZE = 5;
+
 const crypto = require('crypto');
 const start = Date.now();
+const os = require('os');
 
 crypto.pbkdf2('password' , 'any-salt' , 100000 , 512 , 'sha512' , () => {
     console.log('1: ' , Date.now() - start);    
@@ -21,6 +25,10 @@ crypto.pbkdf2('password' , 'any-salt' , 100000 , 512 , 'sha512' , () => {
     console.log('5: ' , Date.now() - start);    
 });
 
-crypto.pbkdf2('password' , 'any-salt' , 100000 , 512 , 'sha512' , () => {
-    console.log('6: ' , Date.now() - start);    
-});
+// crypto.pbkdf2('password' , 'any-salt' , 100000 , 512 , 'sha512' , () => {
+//     console.log('6: ' , Date.now() - start);    
+// });
+
+// console.log(os.cpus());
+
+console.log(process.env.UV_THREADPOOL_SIZE);
